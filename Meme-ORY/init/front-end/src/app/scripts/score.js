87134b89dd4d34df103072ba52a2c9f368332a92
Edmaruu <mamboedwinpau.inchaud@epfedu.fr>
2024-03-23 10:54:50 +0100

    transformation en classe des fonction component de welcome et score

--- Meme-ORY/init/front-end/src/app/scripts/score.js
+++ Meme-ORY/init/front-end/src/app/scripts/score.js
@@ -1,24 +1,25 @@
 
 import template from "../views/score.html";
+import { Component } from "./component";
 import { parseUrl } from "./utils";
-  // TODO #export-functions: export function ScoreComponent
-  // TODO #class: use the ES6 class keyword
+
   /* class ScoreComponent constructor */
-  export function ScoreComponent() {
-    // TODO #extends: call super(template)
+  export class ScoreComponent extends Component {
+    constructor(){
+    super(template)
     let params = parseUrl();
-    // TODO #import-html: assign template to this.template
+
     this.template = template;
     this.name = params.name;
     this.size = parseInt(params.size);
     this.time = parseInt(params.time);
-  }
-
-  // TODO #class: turn function into a method of ScoreComponent
-  /* method ScoreComponent.init */
-  ScoreComponent.prototype.init = function init() {
+    }
+    /* method ScoreComponent.init */
+    init() {
     document.getElementById("name").innerText = this.name;
     document.getElementById("size").innerText = this.size;
     document.getElementById("time").innerText = this.time;
-  };
+  }
+  }
+
 
--- Meme-ORY/init/front-end/src/app/scripts/welcome.js
+++ Meme-ORY/init/front-end/src/app/scripts/welcome.js
@@ -13,7 +13,6 @@
       let form = document.querySelector("form.form-signin");
       form.addEventListener(
         "submit",
-        // TODO #arrow-function: use arrow function instead.
         (event) => {
           event.preventDefault();
           if (form.checkValidity() === false) {
@@ -23,7 +22,7 @@
             let name = event.srcElement.querySelector("#nickname").value;
             let size = parseInt(event.srcElement.querySelector("#size").value);
   
-            _startGame(name, size);
+            this._startGame(name, size);
           }
         },
         false
@@ -31,12 +30,12 @@
   
       return this;
     }
+    _startGame(name, size) {
+
+
+      let gamePage = "./#game";
+    
+      window.location = `${gamePage}?name=${name}&size=${size}`;
+      }
   }
-  function _startGame(name, size) {
-
-
-    let gamePage = "./#game";
-  
-    window.location = `${gamePage}?name=${name}&size=${size}`;
-    }
  
